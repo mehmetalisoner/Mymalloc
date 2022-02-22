bcf13bb5b3eb4c578dc702b6fc4f2a2a351cbb8b
Mehmet Ali Soner <69406231+mehmetalisoner@users.noreply.github.com>
2022-02-21 23:01:38 -0500

    Update mymalloc_aaron.c
    Added some additional comments for better clarity

--- mymalloc_aaron.c
+++ mymalloc_aaron.c
@@ -2,24 +2,24 @@
 #include <stdio.h>
 #include <string.h>
 #include "mymalloc.h"
-#define MEMSIZE 4096                // memory size macro
+#define MEMSIZE 4096                            // memory size macro
 
 
-static short garbage_location = 0xABCD;
-static char memory[MEMSIZE];        // our "heap"/memory block
+static short garbage_location = 0;              // will be used to zero out/delete unused MDM blocks
+static char memory[MEMSIZE];                    // our "heap"/memory block
 
-struct MDM {                        // metadata class, this is how we will store our data into memory
+struct MDM {                                    // metadata class, this is how we will store our data into memory
     struct MDM *prev, *next;
-    int isFree;                     // if usage == 1, then its free, if usage == 0, its occupied
+    int isFree;                                 // if usage == 1, then its free, if usage == 0, its occupied
     unsigned int size;
 };
 
-static struct MDM *global_start = NULL;     // this will be a global pointer that we can refert to. 
-                                            // initially it will be the first block but after mallocing, it will be the last
+static struct MDM *global_start = NULL;         // this will be a global pointer that we can refer to. 
+                                                // initially it will be the first block but after mallocing, it will be the last
 
-void init_memory(){
-    global_start = (struct MDM*) memory[0];
-    global_start->next = NULL;    // this will always be true since this will be the last block
+void init_memory(){                             // this is basically putting in the first block or initializing the memory array
+    global_start = (struct MDM*) &memory[0];
+    global_start->next = NULL;                  // this will always be NULL since this will be the last block
     global_start->prev = NULL;
     global_start->isFree = 1;
     global_start->size = MEMSIZE - sizeof(struct MDM);
@@ -30,19 +30,19 @@ void *get_start_of_data(struct MDM* p)          // might not need this
     return (void*)p + sizeof(struct MDM);
 }
 
-int is_Last(struct MDM* ptr)                    // checks for last block by checking curr.next, -1 == last block, 0 == not last block
+int is_Last(struct MDM* ptr)                    // checks for last block by checking curr.next; return -1 if last block, 0 if not last block
 {
     if (ptr->next == NULL) return -1;   
     else return 0;                             
 }
 
-int pointer_check(void *p)                                              // find *p in array, if we return NULL we have error, if we return a pointer
+int pointer_check(void *p)                                              // find *p in array and see if it points to the start of a block. -1 == not valid, 0 == valid
 {
     struct MDM* curr = global_start;                                    // two pointers to iterate through the memory array, curr = &memory[0] also possible
     struct MDM* prev = NULL;
 
-    while ((curr->next != NULL) || (p != get_start_of_data(curr))) {    // or do a helper function for start_of_data
-        if (p == get_start_of_data(curr)) {                             // if we found the pointer in the memory
+    while ((curr->next != NULL) || (p != get_start_of_data(curr))) {    
+        if (p == get_start_of_data(curr)) {                             // if we found the pointer in the memory at the start of a block
             if (curr->isFree == 1) {                                    // block is already free
                 printf("BLOCK IS ALREADY FREE!");
                 return -1;
@@ -59,7 +59,7 @@ int pointer_check(void *p)                                              // find 
     return -1;
 }
 
-void coalesce(struct MDM *curr, struct MDM *prev)                                                          // coalesce with 3 free blocks, curr in the middle
+void coalesce(struct MDM *curr, struct MDM *prev)                                                          // coalesce with 3 free blocks, curr pointer in the middle
 {
     prev->next = curr->next->next;                                                                         // update pointers
     prev->size += curr->size + curr->next->size + 2 * sizeof(struct MDM);                                  // update size
@@ -74,7 +74,7 @@ void *mymalloc(size_t size, char *file, int line)
     return NULL;
 }
 
-void myfree(void *p, char *file, int line)                              // given a pointer p, free the data associated with p
+void myfree(void *p, char *file, int line)                              // given a VOID pointer p, free the data associated with p
 {
     printf("free called from %s:%d\n", file, line);
 
@@ -98,38 +98,46 @@ void myfree(void *p, char *file, int line)                              // given
     // to the start of the block OR if the block that it is pointing to is already free
 
     
-    if (pointer_check == -1) return;            // pointer is not pointing to a valid address
-    struct MDM* curr = (struct MDM*) p;         // current pointer
-    struct MDM* prev = curr->prev;              // previous pointer
-    curr->isFree = 1;                           // free the block
-
-    // If there is coalesing, then there are two three cases:
+    if (pointer_check == -1) return;            // pointer is not pointing to a valid address, exit program
+    struct MDM* curr = (struct MDM*) p;         // current pointer pointing to p, the block we want to free
+    struct MDM* prev = curr->prev;              // previous pointer, the block before curr
+    curr->isFree = 1;                           // free the block, mark as free
+    
+    
+    
+    // Edge cases when merging blocks:
+    // ---------------------------
     // 1. prev block is free
     // 2. curr.next block is free
     // 3. both are free
+    
+    
+    // Note:
     // we still have to check if .next is the last block because of a NULL pointer exception
-    // by design we don't do any merging if we are freeing the first or the last block
+    // by design we don't do any merging if we are freeing the first or the last block in the memory array
     // they can be merged if the block next to them gets freed
-
-    if (((prev->isFree == 1) && (prev != NULL)) && ((curr->next->isFree == 1) && (curr->next != NULL))) { // both are free and not NULL, do this instead: temp.next != LAST_BLOCK
+    
+    
+    // Checking for NULL in order to avoid NULL pointer exception
+    if ( (prev->isFree == 1) && (prev != NULL) && (curr->next->isFree == 1) && (curr->next != NULL) ) { // both are free and not NULL
         coalesce(curr,prev); // coalesce all three blocks
         return;
     }
 
-    else if ((prev->isFree == 1) && (prev != NULL)) {     // if only previous is free and not null
+    else if ( (prev->isFree == 1) && (prev != NULL) ) {     // if only previous is free and not NULL
         prev->next = curr->next;
         prev->size += curr->size + sizeof(struct MDM);
-        if (is_Last(curr) == 0) curr->next->prev = prev;
-        memcpy((void*)curr,&garbage_location, curr->size + sizeof(struct MDM));
+        if (is_Last(curr) == 0) curr->next->prev = prev;    // do this if curr is not last block
+        memcpy((void*)curr,&garbage_location, curr->size + sizeof(struct MDM));         // zeroing unwanted MDM
         return;
     }
 
-    else if ((curr->next->isFree == 1) && (curr->next != NULL)) {   // if only .next is free and not null
+    else if ( (curr->next->isFree == 1) && (curr->next != NULL) ) {   // if only .next is free and not NULL
         curr->next = curr->next->next;
         curr->next->next->prev = curr; 
         curr->size += curr->next->size + sizeof(struct MDM);
-        // if(is_Last(curr->next) == 0) curr->next->next->prev = curr;      // don't need to check for this because of if statement
-        memcpy((void*)curr->next, &garbage_location,curr->next->size + sizeof(struct MDM));
+        // if(is_Last(curr->next) == 0) curr->next->next->prev = curr;                          // don't need to check for this because of if statement
+        memcpy((void*)curr->next, &garbage_location,curr->next->size + sizeof(struct MDM));     // zeroing unwanted MDM
         return;
     }
 
