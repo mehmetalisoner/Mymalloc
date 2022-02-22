b27900a28d1a9542cbb7e068232caf89c43d5190
Mehmet Ali Soner <69406231+mehmetalisoner@users.noreply.github.com>
2022-02-21 22:58:11 -0500

    Update mymalloc.c
    Added couple suggestions and questions.

--- mymalloc.c
+++ mymalloc.c
@@ -2,12 +2,14 @@
 #include <stdio.h>
 #include "mymalloc.h"
 #define MAXMEM 4096
-static char mem[MAXMEM]={'u'};
+static char mem[MAXMEM]={'u'}; // suggestion: static char mem[MAXMEM]; Check initialized after with a function and variable such as:
+
+// int is_Initialized = 0; First it would be not initialized
 
 MDM *NEXT(MDM* cpointer){
     char *ptr=(char*)cpointer;
-    ptr=ptr+cpointer->size+sizeof(MDM);
-    return (MDM*) ptr;
+    ptr=ptr+cpointer->size+sizeof(MDM);     // suggestion: (MDM*) ptr = cpointer + cpointer->size + sizeof(MDM);
+    return (MDM*) ptr;                      // return ptr;
 }
 
 void *mymalloc(size_t mSize){
@@ -22,12 +24,12 @@ void *mymalloc(size_t mSize){
     printf("hi\n");
     MDM *headptr = (MDM*)mem;
    
-   if(mem[0]=='u'){
+   if(mem[0]=='u'){                                     // suggestion: if(is_Initialized == 0) {initialize the memory, update is_Initialied = 1}
        headptr->allocate='0';
        headptr->size=MAXMEM;
    }
    MDM *cpoint=headptr;
-    while((char*)cpoint< &mem[MAXMEM]){
+    while((char*)cpoint< &mem[MAXMEM]){                             
         //using the cpointer to acess size from the struct 
         //we are saying if the size from the structure 
         //is greater than the size given by client and
@@ -40,11 +42,11 @@ void *mymalloc(size_t mSize){
             cpoint->size=mSize;
             char *rCpt = (char*)cpoint+sizeof(MDM)+mSize;
             printf("we r h\n");
-            if(subtractsize<=sizeof(MDM)){
+            if(subtractsize<=sizeof(MDM)){              // Question: So if there is no more space left, then add the leftover space to cpoint again?
                 printf("no\n");
                 cpoint->size+=subtractsize;
             }
-            else if(rCpt+sizeof(MDM)< &mem[MAXMEM])
+            else if(rCpt+sizeof(MDM)< &mem[MAXMEM])     // Question: Does this put a block right of cpoint if there is enough memory?
             {
                 printf("no2\n");
                 MDM *temporary=(MDM*)rCpt;
@@ -53,14 +55,14 @@ void *mymalloc(size_t mSize){
                 temporary->allocate='0';
 
             }
-            else{
+            else{                                      // suggestion: might not even need this but maybe I am missing an edge case
                 printf("no3\n");
                 cpoint->size+=subtractsize;
             }
             
             rCpt=(char*)cpoint+sizeof(MDM);
             printf("return>>\n");
-            return (void *)rCpt;
+            return (void *)rCpt;               
         }else{
             cpoint=NEXT(cpoint);
         }
