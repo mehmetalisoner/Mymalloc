7c83220b9f1c53a7614f690d017c456aaef69b3e
Mehmet Ali Soner <69406231+mehmetalisoner@users.noreply.github.com>
2022-02-23 14:32:32 -0500

    Update mymalloc_aaron.c
    Latest malloc and free. In this version I am doing pointer arithmetic

--- mymalloc_aaron.c
+++ mymalloc_aaron.c
@@ -1,136 +1,191 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include "mymalloc.h"
-#define MEMSIZE 4096                // memory size macro
+#include "mymalloc.h"   
 
 
-static short garbage_location = 0xABCD;
+
+static short garbage_location = 0x0000;  // garbage location: will used this to delete unwanted MDM blocks
 static char memory[MEMSIZE];        // our "heap"/memory block
+signed short is_Init = -1;         // checks if memory is initialized: -1 == is not, 0 == is initialized
 
-struct MDM {                        // metadata class, this is how we will store our data into memory
-    struct MDM *prev, *next;
-    int isFree;                     // if usage == 1, then its free, if usage == 0, its occupied
-    unsigned int size;
-};
 
-static struct MDM *global_start = NULL;     // this will be a global pointer that we can refert to. 
-                                            // initially it will be the first block but after mallocing, it will be the last
-
-void init_memory(){
-    global_start = (struct MDM*) memory[0];
-    global_start->next = NULL;    // this will always be true since this will be the last block
-    global_start->prev = NULL;
-    global_start->isFree = 1;
-    global_start->size = MEMSIZE - sizeof(struct MDM);
-}
-
-void *get_start_of_data(struct MDM* p)          // might not need this
-{
-    return (void*)p + sizeof(struct MDM);
-}
-
-int is_Last(struct MDM* ptr)                    // checks for last block by checking curr.next, -1 == last block, 0 == not last block
-{
-    if (ptr->next == NULL) return -1;   
-    else return 0;                             
-}
-
-int pointer_check(void *p)                                              // find *p in array, if we return NULL we have error, if we return a pointer
-{
-    struct MDM* curr = global_start;                                    // two pointers to iterate through the memory array, curr = &memory[0] also possible
-    struct MDM* prev = NULL;
-
-    while ((curr->next != NULL) || (p != get_start_of_data(curr))) {    // or do a helper function for start_of_data
-        if (p == get_start_of_data(curr)) {                             // if we found the pointer in the memory
-            if (curr->isFree == 1) {                                    // block is already free
-                printf("BLOCK IS ALREADY FREE!");
-                return -1;
-            }
-            else {                                                      // block is not free
-                printf("BLOCK FOUND! CAN BE FREED NOW");
-                return 0;
-            }
-        }
-        prev = curr;                                                    // update pointers
-        curr = curr->next;
-    }
-    printf("POINTER NOT POINTING TO THE START OF A BLOCK");
-    return -1;
-}
-
-void coalesce(struct MDM *curr, struct MDM *prev)                                                          // coalesce with 3 free blocks, curr in the middle
-{
-    prev->next = curr->next->next;                                                                         // update pointers
-    prev->size += curr->size + curr->next->size + 2 * sizeof(struct MDM);                                  // update size
-    if (is_Last(curr->next) == 0)   curr->next->next->prev = prev;                                         // curr.next.next isnt't last block adjust pointers, if not then do nothing                                         
-    memcpy((void*)curr,&garbage_location,(curr->size + curr->next->size + 2 * sizeof(struct MDM)));        // copy curr and curr.next to garbage location
-    return;
-}
-
-void *mymalloc(size_t size, char *file, int line)
+void *mymalloc(size_t mSize, char *file, int line)
 {
     printf("Mymalloc called from %s:%d\n", file, line);
+
+    // Errors:
+    if(mSize > MEMSIZE){
+        printf("ERROR! (FILE: %s, LINE: %d) -- You cannot allocate a block larger than 4096!\n",file,line);
+        return NULL;
+    }
+    if(mSize <= 0){
+        printf("ERROR! (FILE: %s, LINE: %d) -- Size cannot be zero or lower than zero\n",file,line);
+        return NULL;
+    }
+    MDM *head = (MDM*) &memory[0];
+   
+    if(is_Init == -1){
+       head->isFree = '0';
+       head->size = MEMSIZE;
+       is_Init = 0;
+    }
+
+    MDM *ptr = head;
+    while( (char*)ptr < &memory[MEMSIZE]){
+        if(ptr->size >= mSize && ptr->isFree == '0'){
+            ptr->isFree = '1';                          // not free anymore
+            int leftover_Memory = ptr->size - mSize;
+            ptr->size = mSize;
+            char *rigth_Block = (char*) ptr + sizeof(MDM)+ mSize;
+            if(leftover_Memory <= sizeof(MDM)){
+                ptr->size+=leftover_Memory;
+            }
+            else if(rigth_Block + sizeof(MDM)< &memory[MEMSIZE])
+            {
+                MDM *temp = (MDM*) rigth_Block;
+                temp->size = leftover_Memory - sizeof(MDM);
+                temp->isFree = '0';
+            }
+            void* result = get_start_of_data(ptr);
+            return result;
+        }
+        else {
+            ptr = get_next(ptr);
+        }
+    }
+    printf("ERROR! (FILE: %s, LINE: %d) -- Not enough memory\n ",file,line);
     return NULL;
 }
 
-void myfree(void *p, char *file, int line)                              // given a pointer p, free the data associated with p
+
+
+// get the start of data from a *MDM
+void *get_start_of_data(struct MDM* p)          
 {
-    printf("free called from %s:%d\n", file, line);
+    return (void*) ((char*)p + sizeof(MDM));
+}
 
-    if (p == NULL) {                                                    // check if pointer is a null pointer
-        printf("INVALID POINTER! Given pointer is a null pointer");
+
+// checks for last block by checking curr.next:  -1 == last block, 0 == not last block
+int is_Last(struct MDM* ptr)                    
+{
+    if ((char*) get_next(ptr) > &memory[MEMSIZE]) return -1;   // FIXME: MAKE SURE THIS WORKS
+    else return 0;                             
+}
+
+
+// given a pointer MDM* ptr, get the next block with pointer arithmetic
+struct MDM *get_next(struct MDM* ptr)
+{
+    return (MDM*) ((char*)ptr + ptr->size + sizeof(MDM));
+}
+
+
+// given a pointer MDM* ptr, while freeing, merging with the right free block
+void check_and_merge_rightBlock(MDM* ptr)                                     
+{
+    if (get_next(ptr)->isFree == '0') {    // block right of curr is free
+        MDM* right_Block = get_next(ptr);
+        ptr->size += right_Block->size + sizeof(MDM);
+        memcpy( (void*) right_Block, &garbage_location, right_Block->size + sizeof(MDM) );    // copy unwanted MDM struct "away" by copying in zero
+        printf("BLOCK FREED\n");
+    }
+    else {
+        memcpy( (void*) ptr, &garbage_location, ptr->size + sizeof(MDM));
+        printf("BLOCK FREED\n");
+    }
+}
+
+
+// given a pointer MDM* ptr and MDM* prev, while freeing, merging with the left free block
+void check_and_merge_leftBlock(MDM* ptr, MDM* prev) 
+{
+    if (prev->isFree == '0') {                                                                  // check if prev block is free
+        prev->size += ptr->size + sizeof(MDM);
+        memcpy( (void*) ptr, &garbage_location, ptr->size + sizeof(MDM) );
+        printf("BLOCK FREED\n");
+    }
+    else {
+        MDM* temp = ptr;
+        memcpy( (void*) temp, &garbage_location, temp->size + sizeof(MDM));
+        printf("BLOCK FREED\n");
+    } // FIXME: FREE BLOCK IF PREV IS NOT FREE
+}
+
+
+// given a pointer p, free the data associated with p
+void myfree(void *p, char *file, int line)                              
+{
+    printf("Free called from %s:%d\n", file, line);
+
+    if (p == NULL) {                                                                // check if pointer is a null pointer
+        printf("ERROR! (FILE: %s, LINE: %d) -- INVALID POINTER! Given pointer is a NULL pointer\n",file,line);
         return;
     }
 
-    if ((p > (void*)(memory + MEMSIZE)) || (p < (void*)memory)) {       // check if pointer address is in the memory
-        printf("INVALID POINTER! Given pointer is not in the memory");
+    if ( ((char*) p > (memory + MEMSIZE)) || ((char*) p < memory)) {                // check if pointer address is in the memory
+        printf("ERROR! (FILE: %s, LINE: %d) -- INVALID POINTER! Given pointer is not in the memory\n",file,line);
         return;
     }
 
-    if ((void*) memory == NULL) {                                       // maybe an int isInit and check for that
-        printf("MEMORY NOT INITIALIZED!");
+    if ((char*) memory == NULL) {                                                   // maybe an int isInit and check for that
+        printf("ERROR! (FILE: %s, LINE: %d) --  MEMORY NOT INITIALIZED!\n",file,line);
         return;
     }
 
 
-    // Note: if the pointer passes these edge cases, we still have to check if the pointer is pointing
-    // to the start of the block OR if the block that it is pointing to is already free
+    struct MDM* curr = (MDM*) &memory[0];                                    // two pointers to iterate through the memory array, curr = &memory[0] also possible
+    struct MDM* prev = NULL;
+    unsigned short check = 0;                                                       // boolean to check if address is valid after pointer check
+
+    // POINTER CHECK: check if given pointer has valid address
+
+    while ( (get_next(curr) != NULL) || (p != get_start_of_data(curr))) {      // or do a helper function for start_of_data
+        if (p == get_start_of_data(curr)) {                                       // if we found the pointer in the memory
+            if (curr->isFree == '0') {                                                // block is already free
+                printf("ERROR! (FILE: %s, LINE: %d) --  BLOCK IS ALREADY FREE!\n", file ,line);
+                return;
+            }
+            else {                                                                  // block is not free
+                printf("BLOCK FOUND! CAN BE FREED NOW\n");
+                check = 1;
+                break;                                                              // hold on to the pointers by breaking
+            }
+        }
+        prev = curr;                                                                // update pointers
+        curr = get_next(curr);
+    }
+
+    if (check == 1){  // do nothing
+    }
+    else {
+        printf("ERROR! (FILE: %s, LINE: %d) --  POINTER NOT POINTING TO THE START OF A BLOCK\n", file,line);
+        return;
+    }
+    
+    curr->isFree = '0';                           // free the block
+
+
+
+    // 3 cases while merging:
+    // -----------------------
+    // 1. curr is in the middle randomly, have to merge 3 blocks
+    // 2. curr is the last block, can only merge with left block
+    // 3. curr is the first block, can only merge with the right block 
+
+
+
+    // 1) if curr is first block, check if right block is free and merge them
+    if ( (char*)curr == &memory[0] ) check_and_merge_rightBlock(curr);   
+
 
     
-    if (pointer_check == -1) return;            // pointer is not pointing to a valid address
-    struct MDM* curr = (struct MDM*) p;         // current pointer
-    struct MDM* prev = curr->prev;              // previous pointer
-    curr->isFree = 1;                           // free the block
+    // 2) if curr is first block, check if right block is free and merge them
+    else if ( is_Last(curr) == 0) check_and_merge_leftBlock(curr,prev);
 
-    // If there is coalesing, then there are two three cases:
-    // 1. prev block is free
-    // 2. curr.next block is free
-    // 3. both are free
-    // we still have to check if .next is the last block because of a NULL pointer exception
-    // by design we don't do any merging if we are freeing the first or the last block
-    // they can be merged if the block next to them gets freed
 
-    if (((prev->isFree == 1) && (prev != NULL)) && ((curr->next->isFree == 1) && (curr->next != NULL))) { // both are free and not NULL, do this instead: temp.next != LAST_BLOCK
-        coalesce(curr,prev); // coalesce all three blocks
-        return;
+    
+    // 3) block is in the middle, can merge with right and left block
+    else {
+        check_and_merge_rightBlock(curr);
+        check_and_merge_leftBlock(curr,prev);
     }
-
-    else if ((prev->isFree == 1) && (prev != NULL)) {     // if only previous is free and not null
-        prev->next = curr->next;
-        prev->size += curr->size + sizeof(struct MDM);
-        if (is_Last(curr) == 0) curr->next->prev = prev;
-        memcpy((void*)curr,&garbage_location, curr->size + sizeof(struct MDM));
-        return;
-    }
-
-    else if ((curr->next->isFree == 1) && (curr->next != NULL)) {   // if only .next is free and not null
-        curr->next = curr->next->next;
-        curr->next->next->prev = curr; 
-        curr->size += curr->next->size + sizeof(struct MDM);
-        // if(is_Last(curr->next) == 0) curr->next->next->prev = curr;      // don't need to check for this because of if statement
-        memcpy((void*)curr->next, &garbage_location,curr->next->size + sizeof(struct MDM));
-        return;
-    }
-
 }
