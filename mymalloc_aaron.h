3e5430602a1433a7e712285a8660ecf142e5df19
Mehmet Ali Soner <69406231+mehmetalisoner@users.noreply.github.com>
2022-02-23 14:34:03 -0500

    Update mymalloc_aaron.h
    Latest version of the header file. Small changes

--- mymalloc_aaron.h
+++ mymalloc_aaron.h
@@ -1,19 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#define MEMSIZE 4096 
 #define malloc(s) mymalloc(s, __FILE__, __LINE__)
 #define free(p) myfree(p, __FILE__, __LINE__)
 
 
-struct MDM {                        // metadata class, this is how we will store our data into memory
-    struct MDM *prev, *next;
-    // void* start_of_data;
-    int isFree;                     // if usage == 1, then its free, if usage == 0, its occupied
-    unsigned int size;
+struct MDM {                         // metadata class, this is how we will store our data into memory
+    unsigned int size;               // 4 bytes
+    char isFree;                     // not free == '1', free == '0'. 1 bytes
 };
 
+typedef struct MDM MDM;
 void *mymalloc(size_t size, char *file, int line);
 void myfree(void *p, char *file, int line);
-void init_memory();
-void *get_start_of_data(struct MDM* p);
-int is_Last(struct MDM* p);
-int pointer_check(void *p);
-void coalesce(struct MDM *temp, struct MDM *prev);
+void *get_start_of_data(MDM* p);
+int is_Last(MDM* p);
+struct MDM *get_next(struct MDM* ptr);
+void check_and_merge_rightBlock(MDM* ptr);                                   
+void check_and_merge_leftBlock(MDM* ptr, MDM* prev); 
